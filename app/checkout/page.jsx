64ad3847d10627e7ac31a7ec6b1d08025313d0d0
unknown <abdohani362@gmail.com>
2024-03-12 22:52:54 +0200

    hot fix

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -3,6 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 function page() {
@@ -13,9 +14,11 @@ function page() {
 		amount: Number(searchParams.get("amount")) * 100,
 	};
 	return (
-		<Elements stripe={stripePromise} options={options}>
-			<CheckoutForm amount={Number(searchParams.get("amount")) * 100} />
-		</Elements>
+		<Suspense>
+			<Elements stripe={stripePromise} options={options}>
+				<CheckoutForm amount={Number(searchParams.get("amount")) * 100} />
+			</Elements>
+		</Suspense>
 	);
 }
 
